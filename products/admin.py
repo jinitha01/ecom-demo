062c5d25206854b7ee3ab75edd1306af85f6072a
jinitha01 <joy.jinitha@gmail.com>
2025-07-10 16:14:00 +0900

    Models creation, Set Up Admin Interface

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Product model.
+    """
+    list_display = ('name', 'price', 'description')
+    search_fields = ('name',)
+    list_filter = ('price',)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    """
+    Represents a product in the e-commerce store.
+    """
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class CartItem(models.Model):
+    """
+    Represents an item in the user's shopping cart.
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
